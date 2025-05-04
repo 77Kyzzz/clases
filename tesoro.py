95dc12d0ee0022efc478d3f0de887c1426ab082e
77Kyzzz <b.riveracataldo@gmail.com>
2025-05-04 04:35:47 -0400

    actualizacion tesoro. RESUELTO!

--- tesoro.py
+++ tesoro.py
@@ -1,20 +1,23 @@
 import random
-zona = random.randint(1,50)
-zona_ele = int(input("Ingresa la zona elegida (1-50): "))
 
-intentos = 1
+intentos = 3
+limite1 = int(input("Ingresa el limite 1: "))
+limite2 = int(input("Ingresa el limite 2: "))
+zona_random = random.randint(min(limite1, limite2), max(limite1, limite2))
 
-print(zona)
+while intentos > 0:
+    zona_elegida = int(input(f"Intenta adivinar el numero: ({limite1} - {limite2}): "))
+    print(f"Zona elegida: {zona_elegida}, Zona random: {zona_random}")
 
-while intentos < 3:
-    if zona_ele == zona:
-        print("Tesoro encontrado! felicidades, pirata!")
-        intentos = intentos + 1
-    elif zona_ele.min(zona):
-       print("Muy cerca")
-       intentos = intentos + 1
+    if zona_elegida == zona_random:
+        print("Felicidades Ganaste!")
+        break
+    elif abs(zona_elegida - zona_random) in [1, 2, 3]:
+        print("Muy cerca!")
     else:
-       print("Muy lejos")
-       intentos = intentos + 1
+        print("Muy lejos!")
+    
+    intentos -= 1
 
-print("xd")
+if intentos == 0:
+    print(f"Perdiste nt. la zona era: {zona_random}")
